d9cc1126ff2ca87624c325c0108220287f40c061
trou <144759552+trouDev@users.noreply.github.com>
2025-03-09 22:50:47 -0700

    SimulationEventQueue

--- Project_3/Queue/QueuePackage/SimulationEventQueue.java
+++ Project_3/Queue/QueuePackage/SimulationEventQueue.java
@@ -1,46 +1,55 @@
 package QueuePackage;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class SimulationEventQueue implements SimulationEventQueueInterface {
 
-    private float simulationTime;
-    private SimulationEvent[] queue;
+    private double simulationTime;
+    private PriorityQueue<SimulationEvent> queue;
+
+    public SimulationEventQueue() {
+        queue = new PriorityQueue<>((e1,e2) -> Double.compare(e1.getTime(), e2.getTime()));
+        simulationTime = 0.0;
+    }
     @Override
     public void add(SimulationEvent newEntry) {
-
+        if (newEntry.getTime() >= simulationTime) {
+            queue.add(newEntry);
+        }
     }
 
     @Override
     public SimulationEvent remove() {
-
-        return null;
+        SimulationEvent next = queue.poll();
+        if (next != null) {
+            simulationTime = next.getTime();
+        }
+        return next;
     }
 
     @Override
     public SimulationEvent peek() {
-
-        return null;
+        return queue.peek();
     }
 
     @Override
     public boolean isEmpty() {
-
-        return true;
+        return queue.isEmpty();
     }
 
     @Override
     public int getSize() {
-
-        return -1;
+        return queue.size();
     }
 
     @Override
     public void clear() {
-
+        queue.clear();
     }
 
     @Override
     public double getCurrentTime() {
-
-        return -1.1;
+        return simulationTime;
     }
 }
